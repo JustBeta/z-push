ec9854acdf7ecaf36e58d6f0d3f10e4810d188e7
justbeta <jbarone@free.fr>
2025-11-11 06:49:42 +0100

    Update docker-run.sh

--- root/usr/local/bin/docker-run.sh
+++ root/usr/local/bin/docker-run.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# /bin/sh -c /usr/local/bin/docker-run.sh
+
 mkdir -p /state /var/log/z-push
 touch /var/log/z-push/z-push-error.log /var/log/z-push/z-push.log
 
@@ -10,14 +12,26 @@ cp /etc/supervisord.conf.dist /etc/supervisord.conf
 
 sed -e "s/define('BACKEND_PROVIDER', '')/define('BACKEND_PROVIDER', 'BackendIMAP')/" \
     -e "s|define('STATE_DIR', '/var/lib/z-push/')|define('STATE_DIR', '/state/')|" \
+    -e "s|define('USE_X_FORWARDED_FOR_HEADER', false)|define('USE_X_FORWARDED_FOR_HEADER', "$USE_X_FORWARDED_FOR_HEADER")|" \
     -e "s|define('TIMEZONE', '')|define('TIMEZONE', '"$TIMEZONE"')|" /opt/zpush/config.php.dist > /opt/zpush/config.php
 #    -e "s|define('LOGFILEDIR', '/var/log/z-push/')|define('LOGFILEDIR', '/data/logs/')|" \
 
 sed -e "s/define('IMAP_SERVER', 'localhost')/define('IMAP_SERVER', '"$IMAP_SERVER"')/" \
     -e "s/define('IMAP_PORT', 143)/define('IMAP_PORT', '"$IMAP_PORT"')/" \
-    -e "s|define('IMAP_OPTIONS', '/notls/norsh')|define('IMAP_OPTIONS', '/tls/norsh/novalidate-cert')|" \
+    -e "s|define('IMAP_OPTIONS', '/notls/norsh')|define('IMAP_OPTIONS', '"$IMAP_OPTIONS"')|" \
+    -e "s|define('IMAP_FOLDER_CONFIGURED', true)|define('IMAP_FOLDER_CONFIGURED', "$IMAP_FOLDER_CONFIGURED")|" \
+    -e "s|define('IMAP_FOLDER_PREFIX', '')|define('IMAP_FOLDER_PREFIX', '"$IMAP_FOLDER_PREFIX"')|" \
+    -e "s|define('IMAP_FOLDER_PREFIX_IN_INBOX', false)|define('IMAP_FOLDER_PREFIX_IN_INBOX', "$IMAP_FOLDER_PREFIX_IN_INBOX")|" \
+    -e "s|define('IMAP_FOLDER_INBOX', 'INBOX')|define('IMAP_FOLDER_INBOX', '"$IMAP_FOLDER_INBOX"')|" \
+    -e "s|define('IMAP_FOLDER_SENT', 'SENT')|define('IMAP_FOLDER_SENT', '"$IMAP_FOLDER_SENT"')|" \
+    -e "s|define('IMAP_FOLDER_DRAFT', 'DRAFTS')|define('IMAP_FOLDER_DRAFT', '"$IMAP_FOLDER_DRAFT"')|" \
+    -e "s|define('IMAP_FOLDER_TRASH', 'TRASH')|define('IMAP_FOLDER_TRASH', '"$IMAP_FOLDER_TRASH"')|" \
+    -e "s|define('IMAP_FOLDER_SPAM', 'SPAM')|define('IMAP_FOLDER_SPAM', '"$IMAP_FOLDER_SPAM"')|" \
+    -e "s|define('IMAP_FOLDER_ARCHIVE', 'ARCHIVE')|define('IMAP_FOLDER_ARCHIVE', '"$IMAP_FOLDER_ARCHIVE"')|" \
+    -e "s|define('IMAP_INLINE_FORWARD', true)|define('IMAP_INLINE_FORWARD', "$IMAP_INLINE_FORWARD")|" \
+    -e "s|define('IMAP_EXCLUDED_FOLDERS', '')|define('IMAP_EXCLUDED_FOLDERS', '"$IMAP_EXCLUDED_FOLDERS"')|" \
     -e "s/define('IMAP_SMTP_METHOD', 'mail')/define('IMAP_SMTP_METHOD', 'smtp')/" \
-    -e "s|imap_smtp_params = array()|imap_smtp_params = array('host' => '"$SMTP_SERVER"', 'port' => '"$SMTP_PORT"', 'auth' => true, 'username' => 'imap_username', 'password' => 'imap_password', 'verify_peer_name' => false, 'verify_peer' => false, 'allow_self_signed' => true)|" \
+    -e "s|imap_smtp_params = array()|imap_smtp_params = array('host' => '"$SMTP_SERVER"', 'port' => '"$SMTP_PORT"')|" \
     -e "s/define('IMAP_FOLDER_CONFIGURED', false)/define('IMAP_FOLDER_CONFIGURED', true)/" /opt/zpush/backend/imap/config.php.dist > /opt/zpush/backend/imap/config.php
 
 [ -f "/config/config.php" ] && cat /config/config.php >> /opt/zpush/config.php
